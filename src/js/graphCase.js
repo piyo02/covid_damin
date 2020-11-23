771c634d91432e57cf7ad5142d094add6ec20afe
piyo02 <alzidni2000@gmail.com>
2020-11-23 12:31:25 +0800

    [ush

--- src/js/graphCase.js
+++ src/js/graphCase.js
@@ -7,54 +7,34 @@ $(document).ready(function() {
         const labels = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September'];
         let dataIndonesia = [], dataFilipina = [], dataMalaysia = [], dataSingapura = [];
 
-        results[0].forEach(data => {
-            dataIndonesia.push(data.cumulative_cases);
-        })
-
-        results[1].forEach(data => {
-            dataFilipina.push(data.cumulative_cases);
-        })
-
-        results[2].forEach(data => {
-            dataMalaysia.push(data.cumulative_cases);
-        })
-        
-        results[3].forEach(data => {
-            dataSingapura.push(data.cumulative_cases);
-        })
+        results[0].forEach(data => { dataIndonesia.push(data.cumulative_cases); })
+        results[1].forEach(data => { dataFilipina.push(data.cumulative_cases); })
+        results[2].forEach(data => { dataMalaysia.push(data.cumulative_cases); })
+        results[3].forEach(data => { dataSingapura.push(data.cumulative_cases); })
 
         var ctx = document.getElementById('chartCase').getContext('2d');
         var chart = new Chart(ctx, {
             type: 'line',
-
             data: {
                 labels: labels,
                 datasets: [
-                    {
-                    label: 'Indonesia',
-                    backgroundColor: 'rgba(0, 0, 0, 0)',
-                    borderColor: '#17a2b8',
-                    data: dataIndonesia
-                    },
-                    {
-                    label: 'Filipina',
-                    backgroundColor: 'rgba(0, 0, 0, 0)',
-                    borderColor: '#6c757d',
-                    data: dataFilipina
-                    },
-                    {
-                    label: 'Malaysia',
-                    backgroundColor: 'rgba(0, 0, 0, 0)',
-                    borderColor: '#343a40',
-                    data: dataMalaysia
-                    },
-                    {
-                    label: 'Singapura',
-                    backgroundColor: 'rgba(0, 0, 0, 0)',
-                    borderColor: '#28a745',
-                    data: dataSingapura
-                    }
-                ]
+                            {   label: 'Indonesia',
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                                borderColor: '#17a2b8',
+                                data: dataIndonesia },
+                            {   label: 'Filipina',
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                                borderColor: '#6c757d',
+                                data: dataFilipina },
+                            {   label: 'Malaysia',
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                                borderColor: '#343a40',
+                                data: dataMalaysia },
+                            {   label: 'Singapura',
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                                borderColor: '#28a745',
+                                data: dataSingapura }
+                            ]
             },
             options: {}
         });
